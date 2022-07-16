0204d0162afccb3c5cd51871ae75544ed33941a4
Jose Augusto <joseaugustocustodio@hotmail.com.br>
2022-07-15 23:54:53 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from './Componets/NotificationButton'
+
+
 function App() {
   return (
-    <h1>ola</h1>
+    <>
+      <h1>ola</h1>
+      <NotificationButton />
+    </>
+
   )
 }
 
